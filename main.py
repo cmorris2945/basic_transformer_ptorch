d7542337b0cc43726c3653b0e0f98866b466ecbf
cmorris2945 <cmorris2945@yahoo.com>
2023-04-21 09:59:07 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert(self.heads_dim * heads == embed_size), "Embedding size has to be divisible by the heads..."
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len,query_len =
